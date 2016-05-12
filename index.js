d9e4fc2a8bea0ce65c1bb01615c8593858904777
Blaine Bublitz <blaine.bublitz@gmail.com>
2016-05-11 18:16:45 -0700

    Update: Follow angular pattern of returning promise

--- index.js
+++ index.js
@@ -3,47 +3,43 @@ var Q = require('q');
 var readFile = Q.denodeify(require('fs').readFile);
 var resolve = require('path').resolve;
 
-function presetOpts(cb) {
-  var parserOpts = {
-    headerPattern: /^(\w*)\: (.*?)(?:\((.*)\))?$/,
-    headerCorrespondence: [
-      'tag',
-      'message'
-    ]
-  };
+var parserOpts = {
+  headerPattern: /^(\w*)\: (.*?)(?:\((.*)\))?$/,
+  headerCorrespondence: [
+    'tag',
+    'message'
+  ]
+};
 
-  var writerOpts = {
-    transform: function(commit) {
-      if (!commit.tag || typeof commit.tag !== 'string') {
-        return;
-      }
+var writerOpts = {
+  transform: function(commit) {
+    if (!commit.tag || typeof commit.tag !== 'string') {
+      return;
+    }
 
-      if (typeof commit.hash === 'string') {
-        commit.hash = commit.hash.substring(0, 7);
-      }
+    if (typeof commit.hash === 'string') {
+      commit.hash = commit.hash.substring(0, 7);
+    }
 
-      return commit;
-    },
-    groupBy: 'tag',
-    commitGroupsSort: 'title',
-    commitsSort: ['tag', 'message']
-  };
+    return commit;
+  },
+  groupBy: 'tag',
+  commitGroupsSort: 'title',
+  commitsSort: ['tag', 'message']
+};
 
-  Q.all([
-    readFile(resolve(__dirname, 'templates/template.hbs'), 'utf-8'),
-    readFile(resolve(__dirname, 'templates/header.hbs'), 'utf-8'),
-    readFile(resolve(__dirname, 'templates/commit.hbs'), 'utf-8')
-  ])
-    .spread(function(template, header, commit) {
-      writerOpts.mainTemplate = template;
-      writerOpts.headerPartial = header;
-      writerOpts.commitPartial = commit;
+module.exports = Q.all([
+  readFile(resolve(__dirname, 'templates/template.hbs'), 'utf-8'),
+  readFile(resolve(__dirname, 'templates/header.hbs'), 'utf-8'),
+  readFile(resolve(__dirname, 'templates/commit.hbs'), 'utf-8')
+])
+  .spread(function(template, header, commit) {
+    writerOpts.mainTemplate = template;
+    writerOpts.headerPartial = header;
+    writerOpts.commitPartial = commit;
 
-      cb(null, {
-        parserOpts: parserOpts,
-        writerOpts: writerOpts
-      });
-    });
-}
-
-module.exports = presetOpts;
+    return {
+      parserOpts: parserOpts,
+      writerOpts: writerOpts
+    };
+  });
